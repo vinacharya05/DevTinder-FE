eed5762a5cc3b3de0f2083d3feef3ebae027e110
Vinaya Acharya <vinay.acharya05@gmail.com>
2026-01-10 12:22:35 +0530

    Implement logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
     const [emailId, setEmailId] = useState("vinay@gmail.com");
     const [password, setPassword] = useState("Adacardano@07#");
+    const [error, setError] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
     
@@ -21,6 +22,7 @@ const Login = () => {
             dispatch(addUser(loggedInUser.data));
             return navigate("/");
         } catch(err) {
+            setError(err?.response?.data || 'Something went wrong');
             console.log(err);
         }
     }
@@ -41,6 +43,7 @@ const Login = () => {
                             <input type="text" className="input" value={password} onChange={(e) => setPassword(e.target.value)}/>
                         </fieldset>
                     </div>
+                    <p class="text-red-500">{error}</p>
                     <div className="card-actions justify-center m-2">
                         <button className="btn btn-primary" onClick={handleLogin}>Login</button>
                     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,24 @@
-import { useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from 'react-router-dom';
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
     const user = useSelector((store) => store.user);
-    console.log(user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const logout = async () => {
+        try {
+           await axios.post(BASE_URL + "/logout", {}, {withCredentials: true});
+           dispatch(removeUser());
+           navigate('/login');
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className="navbar bg-base-300 shadow-sm">
             <div className="flex-1">
@@ -31,7 +46,7 @@ const NavBar = () => {
                                     </Link>
                                 </li>
                                 <li><a>Settings</a></li>
-                                <li><a>Logout</a></li>
+                                <li><a onClick={logout}>Logout</a></li>
                             </ul>
                         </div>
                     </div>
