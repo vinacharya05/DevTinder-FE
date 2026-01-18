ef99e03008af54db7988ea2f2f5556bab9fe34b8
Vinaya Acharya <vinay.acharya05@gmail.com>
2026-01-18 20:15:12 +0530

    Make changes to socket endpoints to work on production

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,5 +3,10 @@ import { BASE_URL } from "./constants"
 
 
 export const createSocketConnection = () => {
-    return io(BASE_URL)
+    if (location.hostname === 'localhost') {
+        return io(BASE_URL);
+    } else {
+        return io('/', {path: '/api/socket.io'})
+    }
+
 }
